c508b5368e6e56be139ef6b254d735a8e28bded5
biorev <media@biorev.us>
2021-06-04 12:53:47 +0530

    intial commit

---
+++ src/Fivenine.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Biorev\Fivenine;
+
+class Fivenine
+{
+    public function getName($var = null)
+    {
+        return 'This is '. $var.' returned from fivenine package';
+    }
+    
+}
